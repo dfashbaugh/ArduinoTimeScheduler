62f58e9b9fb7bd3a623b1e8f4eb2a1f6afcbf6f6
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-01-15 21:56:37 -0500

    Wrote a second function to test the scheduler with

--- testScheduler.cpp
+++ testScheduler.cpp
@@ -8,12 +8,18 @@ void TestMe()
 	cout << "TESTED";
 }
 
+void TestThis()
+{
+	cout << "Test This!";
+}
+
 FunctionScheduler myScheduler(3);
 
 int main(void)
 {
 	cout << "Hello" << endl;
 	myScheduler.AddFunction(TestMe, 10);
+	myScheduler.AddFunction(TestThis, 10);
 
 	for(int i = 0; i < 30; i++)
 	{
@@ -21,7 +27,6 @@ int main(void)
 		myScheduler.RunFunctions(i);
 		cout << endl;
 	}
-	
 
 	return 0;
 }
