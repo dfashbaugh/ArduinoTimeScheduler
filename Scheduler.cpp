1758848664470ee5dc2e75ec23cce7dc291815f7
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-01-15 22:54:56 -0500

    Capped the number of functions you can add to the number of functions the object was constructed with

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -4,17 +4,21 @@ FunctionScheduler::FunctionScheduler(int numFunctions)
 {
 	FuncArray = new FunctionAndTime[numFunctions];
 	curMaxFunctions = 0;
+	maxFunctions = numFunctions;
 }
 
 void FunctionScheduler::AddFunction(funcToRepeat function, unsigned long interval)
 {
-	FunctionAndTime newFuncAndTime;
-	newFuncAndTime.theFunction = function;
-	newFuncAndTime.interval = interval;
-	newFuncAndTime.lastTime = 0;
+	if(curMaxFunctions < maxFunctions)
+	{
+		FunctionAndTime newFuncAndTime;
+		newFuncAndTime.theFunction = function;
+		newFuncAndTime.interval = interval;
+		newFuncAndTime.lastTime = 0;
 
-	FuncArray[curMaxFunctions] = newFuncAndTime;
-	curMaxFunctions++;
+		FuncArray[curMaxFunctions] = newFuncAndTime;
+		curMaxFunctions++;
+	}
 }
 
 void FunctionScheduler::RunFunctions(unsigned long testTime)
--- Scheduler.h
+++ Scheduler.h
@@ -23,4 +23,5 @@ public:
 private:
 	FunctionAndTime* FuncArray;
 	int curMaxFunctions;
+	int maxFunctions;
 };
