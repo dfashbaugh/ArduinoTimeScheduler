abbf31ff4b29e478e69af0c5479c00fdbd33c262
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-01-15 21:55:11 -0500

    Created a more readable debug output for testing purposes

--- testScheduler.cpp
+++ testScheduler.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void TestMe()
 {
-	cout << "TESTED" << endl;
+	cout << "TESTED";
 }
 
 FunctionScheduler myScheduler(3);
@@ -15,7 +15,13 @@ int main(void)
 	cout << "Hello" << endl;
 	myScheduler.AddFunction(TestMe, 10);
 
-	myScheduler.RunFunctions(11);
+	for(int i = 0; i < 30; i++)
+	{
+		cout << "Outputs At " << i << ": ";
+		myScheduler.RunFunctions(i);
+		cout << endl;
+	}
+	
 
 	return 0;
 }
